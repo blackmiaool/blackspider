969c26820863dfc6527524845ce7d9b25c85e17e
blackmiaool <blackmiaool@gmail.com>
2018-01-06 15:43:52 +0800

    use ajax

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var http = require('http');
 const { URL } = require('url');
 const images = require('./test2');
-const fs=require('fs-extra');
-const getIp=require('./getip');
+const fs = require('fs-extra');
+const getIp = require('./getip');
 
-const port=33326;
-const nginxPort=33327;
+const port = 33326;
+const nginxPort = 33327;
 function download(source, path) {
     return new Promise((resolve) => {
         const file = fs.createWriteStream(path);
@@ -44,20 +44,20 @@ async function task() {
     for (let i = 0; i < images.length; i++) {
         await download(images[i], `result/${i + 1}.jpg`);
         console.log(i);
-    } 
+    }
 }
 
 var finalhandler = require('finalhandler')
 var serveStatic = require('serve-static')
- 
+
 // Serve up public/ftp folder
-var serve = serveStatic('./result', {'index': ['index.html', 'index.htm']})
- 
+var serve = serveStatic('./result', { 'index': ['index.html', 'index.htm'] })
+
 // Create server
-var server = http.createServer(function onRequest (req, res) {
-  serve(req, res, finalhandler(req, res))
+var server = http.createServer(function onRequest(req, res) {
+    serve(req, res, finalhandler(req, res))
 })
- 
+
 // Listen
 server.listen(nginxPort)
 function createPDF(sourcePath, length, targetPath) {
@@ -82,24 +82,24 @@ function createPDF(sourcePath, length, targetPath) {
 }
 // createPDF('result/1514797855093',217,'result/1514797855093/1514797855093.pdf');
 
-const io = require('blacksocket.io/server')(port, {
-    path: '/test'
-});
-io.on('connection', function (socket) {
-    socket.on('task', async (list) => {
-        const time = Date.now();
-        const targetFolder=`result/${time}`;
+// const io = require('blacksocket.io/server')(port, {
+//     path: '/test'
+// });
+// io.on('connection', function (socket) {
+//     socket.on('task', async (list) => {
+//         const time = Date.now();
+//         const targetFolder = `result/${time}`;
 
-        await fs.ensureDir(targetFolder);
-        for (let i = 0; i < list.length; i++) {
-            await download(list[i], `${targetFolder}/${i + 1}.jpg`);
-            socket.emit('progress', i+'');
-        }
-        const target = `${targetFolder}.pdf`;
-        createPDF(targetFolder, list.length, target);
-        return `http://${getIp()[0]}:${nginxPort}/${target}`;
-    });
-});
+//         await fs.ensureDir(targetFolder);
+//         for (let i = 0; i < list.length; i++) {
+//             await download(list[i], `${targetFolder}/${i + 1}.jpg`);
+//             socket.emit('progress', i + '');
+//         }
+//         const target = `${targetFolder}.pdf`;
+//         createPDF(targetFolder, list.length, target);
+//         return `http://${getIp()[0]}:${nginxPort}/${target}`;
+//     });
+// });
 // process.on('uncaughtException', (err) => {
 //     console.error(err.stack);
 //     console.log('Node NOT Exiting...');
@@ -109,3 +109,50 @@ io.on('connection', function (socket) {
 //     // application specific logging, throwing an error, or other logic here
 // });
 console.log(getIp());
+
+
+const Koa = require('koa');
+const app = new Koa();
+var bodyParser = require('koa-bodyparser');
+var exec = require('child_process').exec;
+var sys = require('util')
+
+app.use(bodyParser());
+// response
+let progressMap = {};
+app.use(async ctx => {
+    const params = ctx.request.body;
+    console.log(params);
+    console.log(ctx.path)
+    ctx.set('Access-Control-Allow-Origin', ctx.request.header.origin);
+
+    if (ctx.path === "/task") {
+        console.log(1);
+        const time = Date.now();
+        const targetFolder = `result/${time}`;
+        const { list, id } = params;
+        async function doDownload() {
+            await fs.ensureDir(targetFolder);
+            for (let i = 0; i < list.length; i++) {
+                console.log(list[i], `${targetFolder}/${i + 1}.jpg`);
+                await download(list[i], `${targetFolder}/${i + 1}.jpg`);
+                progressMap[id] = `${i}/${list.length}`;
+            }
+            const target = `${targetFolder}.pdf`;
+            createPDF(targetFolder, list.length, target);
+            const finalUrl = `http://${getIp()[0]}:${nginxPort}/${target}`;
+            progressMap[id] = finalUrl;
+        }
+        if (!progressMap[id]) {
+            progressMap[id] = `0/${list.length}`;
+            doDownload();
+        }
+        ctx.body = JSON.stringify({ code: 0 });
+
+    } else if (ctx.path === '/progress') {
+        const { id } = params;
+        ctx.body = progressMap[id];
+    }
+});
+
+app.listen(25334);
